0d2e9ac362dbd292e64fe32202d54329fdfb65c9
mivolpe <michele.volpe@helb-prigogine.be>
2021-11-12 16:01:40 +0100

    feat : add swipe card + logical

--- StackNavigator.js
+++ StackNavigator.js
@@ -4,19 +4,29 @@ import useAuth from './hooks/useAuth';
 import ChatSCreen from './screens/ChatScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginScreen from './screens/LoginScreen';
+import ModalScreen from './screens/ModalScreen';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
     const { user }= useAuth();
     return (
-        <Stack.Navigator
+        <Stack.Navigator 
+            screenOptions={{ 
+                headerShown: false,
+             }}
         >
 
             {user ? (
                 <>
-                    <Stack.Screen name="Home" component={HomeScreen}/>
-                    <Stack.Screen name="Chat" component={ChatSCreen}/>
+                    <Stack.Group>
+                        <Stack.Screen name="Home" component={HomeScreen}/>
+                        <Stack.Screen name="Chat" component={ChatSCreen}/>
+                    </Stack.Group>
+                    <Stack.Group screenOptions={{ presentation: "modal" }}>
+                        <Stack.Screen name="Modal" component={ModalScreen}/>
+                    </Stack.Group>
+
                 </>
             ) : (
                 <Stack.Screen name="Login" component={LoginScreen}/>
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,23 +1,210 @@
 import { useNavigation } from '@react-navigation/core';
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+import React, { useEffect, useLayoutEffect, useRef, useState } from 'react';
+import { View, Text, Button, TouchableOpacity, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import useAuth from '../hooks/useAuth';
+import tw from 'tailwind-rn';
+import { AntDesign, Entypo, Ionicons } from "@expo/vector-icons";
+import Swiper from 'react-native-deck-swiper';
+import { collection, doc, getDocs, onSnapshot, query, setDoc, where } from '@firebase/firestore';
+import { db } from '../firebase';
+
 
 const HomeScreen = () => {
     const navigation = useNavigation();
-    const { logout } = useAuth()
+    const { user,logout } = useAuth();
+    const [profiles, setProfiles] = useState([]);
+    const swipeRef = useRef(null);
+
+   useLayoutEffect(() => {
+        onSnapshot(doc(db, "users", user.uid), snapshot => {
+            if (!snapshot.exists()){
+                navigation.navigate("Modal");
+            }
+        });
+   }, []);
+
+   useEffect(() => {
+    let unsub;
+    
+    const fetchCards = async () => {
+
+        const passes = await getDocs(collection(db, "users", user.uid, "passes")).then(
+            (snapshot) => snapshot.docs.map((doc) => doc.id)
+        );
+
+        const swipes = await getDocs(collection(db, "users", user.uid, "swipes")).then(
+            (snapshot) => snapshot.docs.map((doc) => doc.id)
+        );
+
+        const passedUserIds = passes.length > 0 ? passes : ["test"];
+        const swipedUserIds = swipes.length > 0 ? passes : ["test"];
+
+
+        unsub = onSnapshot(query(collection(db,"users"),where("id", "not-in", [...passedUserIds, ...swipedUserIds])), snapshot => {
+            setProfiles(
+                snapshot.docs.filter(doc => doc.id !== user.uid).map((doc) => ({
+                    id: doc.id,
+                    ...doc.data()
+                }))
+            );
+        });
+    };
+
+    fetchCards();
+    return unsub;
+    }, [db]);
+
+    const swipeLeft = async() =>{
+        if(!proifles[cardIndex]) return;
+
+        const userSwiped = profiles[cardIndex];
+        setDoc(doc(db, "users", user.uid, "passes", userSwiped.id),userSwiped);
+    };
+
+    const swipeRight = async() =>{
+        if(!proifles[cardIndex]) return;
+
+        const userSwiped = profiles[cardIndex];
+        setDoc(doc(db, "users", user.uid, "swipes", userSwiped.id),userSwiped);
+
+    };
 
     return (
-        <View>
-            <Text>I am the Homescreen</Text>
-            <Button
-                title="Go to Chat Screen"
-                onPress={() => navigation.navigate('Chat')}
-            />
+        <SafeAreaView
+            style={tw("flex-1")}
+        >
+            {/* Header */}
+                <View
+                    style={tw("flex-row items-center justify-between px-5")}
+                >
+                    <TouchableOpacity>
+                        <Image 
+                            style={tw("h-10 w-10 rounded-full")} 
+                            source={{ uri: user.photoURL }}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("Modal")}
+                    >
+                        <Image
+                            style={tw("h-14 w-14")}
+                            source={require("../logo.png")}
+                        />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate("Chat")}
+                    >
+                        <Ionicons 
+                            name='chatbubbles'
+                            size={30}
+                            color="#FF5864"
+                        />
+                    </TouchableOpacity>
+                </View>
+            {/* End of Header */}
 
-            <Button title="logout" onPress={logout} />
-        </View>
-    )
+            {/* Cards */}
+
+            <View
+                style={tw("flex-1 -mt-6")}
+            >
+                <Swiper
+                    ref={swipeRef}
+                    containerStyle={{ backgroundColor: "transparent"}}
+                    cards={profiles}
+                    animateCardOpacity
+                    verticalSwipe={false}
+                    onSwipedLeft={(cardIndex) => {
+                        swipeLeft(cardIndex);
+                    }}
+                    onSwipedRight={(cardIndex) => {
+                        swipeRight(cardIndex);
+                    }}
+                    overlayLabels={{
+                        left: {
+                            title: "NOPE",
+                            style: {
+                                label: {
+                                    textAlign: "right",
+                                    color: "red",
+                                },
+                            },
+                        },
+                        right: {
+                            title: "LIKE",
+                            style: {
+                                label: {
+                                    color: "green",
+                                    textAlign: "left",
+                                },
+                            },
+                        },
+                    }}
+                    renderCard={(card) => card ? (
+                        <View
+                            style={tw("bg-white-500 h-5/6 rounded-xl")}
+                        >
+                            <Image 
+                                style={tw("absolute top-0 h-full w-full rounded-xl")}
+                                source={{ uri: card.photoURL }}
+                            />
+                            <View style={tw("absolute bottom-0 flex-row justify-between bg-transparent h-20 px-5")}>
+                                <View>
+                                    <Text style={tw("text-xl font-bold text-white")}>
+                                        {card.displayName}
+                                    </Text>
+                                    <Text> job </Text>
+                                    <Text> 
+                                        <Ionicons 
+                                            name='location'
+                                            size={16} 
+                                        />
+                                        distance 
+                                    </Text>
+                                </View>
+                                <Text style={tw("text-xl px-3 text-white")}>
+                                     23
+                                </Text>
+                            </View>
+
+                        </View>
+                    ) : (
+                        <View
+                            style={tw("relative bg-white h-5/6 rounded-xl justify-center items-center")}
+                        >
+                            <Text
+                                style={tw("font-bold pb-5")}
+                            >
+                                No more profiles
+                            </Text>
+                            <Image
+                                style={tw("h-20 w-20")}
+                                height={100}
+                                width={100}
+                                source={{ uri: "https://links.papareact.com/6gb" }}
+                            />
+                        </View>
+                    )}
+                />
+            </View>
+            <View style={tw("flex flex-row justify-evenly bottom-4")}>
+                <TouchableOpacity 
+                    onPress={() => swipeRef.current.swipeLeft()}
+                    style={tw("items-center justify-center rounded-full w-16 h-16 bg-red-200")}
+                >
+                    <Entypo name="cross"  size={30} color="red" />
+                </TouchableOpacity>
+                <TouchableOpacity 
+                    onPress={() => swipeRef.current.swipeRight()}
+                    style={tw("items-center justify-center rounded-full w-16 h-16 bg-green-200")}
+                >
+                    <AntDesign name="heart" size={30} color="green" />
+                </TouchableOpacity>
+            </View>               
+            {/* End of Cards */}
+        </SafeAreaView>
+    );
 }
 
 export default HomeScreen
